91c4177638702983b7461737d4ba8c81d1c1502b
Rafael Gomez <rafaelgomez@MacBook-Air-de-Rafael.local>
2025-03-27 00:30:12 -0300

    fix

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -4,8 +4,8 @@ import React, { createContext, useContext, useState, useEffect, ReactNode } from
 import { io, Socket } from 'socket.io-client';
 import { Message, ChatContextType } from '@/types/chat';
 
-const SOCKET_SERVER_URL = 'http://localhost:4000';
-//const SOCKET_SERVER_URL = 'https://chat-backend-6r2a.onrender.com';
+//const SOCKET_SERVER_URL = 'http://localhost:4000';
+const SOCKET_SERVER_URL = 'https://chat-backend-6r2a.onrender.com';
 const ChatContext = createContext<ChatContextType | undefined>(undefined);
 
 export const ChatProvider = ({ children }: { children: ReactNode }) => {
